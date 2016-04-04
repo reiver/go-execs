6a73b6d87dcb8c3f6fbd7c44b38b85d0785c43f9
Charles Iliya Krempeaux <iamreiver@gmail.com>
2016-04-04 00:05:08 -0700

    added docs

--- doc.go
+++ doc.go
@@ -2,7 +2,7 @@
 Package execs provides pipelining capabilities to both internal and external commands,
 as well a convenient means of inlining internal commands.
 
-Package is execs is meant to work with the "os/exec" package that is part of the Go standard library.
+Package execs is meant to work with the "os/exec" package, that is part of the Go standard library.
 
 Example Usage
 
